7490fc558ca5c6626989289617f3c7cba5243bbf
Łukasz Zięba <l.zieba@i-systems.pl>
2022-05-16 20:59:15 +0200

    Auth stuff end

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -1,9 +1,10 @@
 import { Middleware } from 'type-graphql/dist/interfaces/Middleware';
 import { MyContext } from '@types';
+import { ApolloError } from 'apollo-server-express';
 
 export const isAuth: Middleware<MyContext> = ({ context }, next) => {
     if (!context.req.session.userId) {
-        throw new Error('User is not autjticated');
+        throw new ApolloError('User is not authenticated', 'FORBIDDEN');
     }
 
     return next();
